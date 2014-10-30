e1fdd69c4f8ff28b9d474b47bec549b813217b6c
Paul Horn <knutwalker@gmail.com>
2014-10-30 15:48:32 +0100

    Add support for reactive streams

---
+++ contrib/reactive-streams/src/main/scala/scala/transducers/contrib/AtomicSubscription.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2014 Paul Horn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scala.transducers.contrib
+
+import java.util.concurrent.atomic.AtomicReference
+
+import org.reactivestreams.Subscription
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
+class AtomicSubscription extends Subscription {
+  private val underlying = new AtomicReference[Subscription]
+
+  private var stash = Queue.empty[Subscription ⇒ Unit]
+
+  def set(s: Subscription): Unit =
+    if (underlying.compareAndSet(null, s)) {
+      unstash(stash, s)
+    }
+
+  def request(n: Long): Unit =
+    doOnUnderlying(_.request(n))
+
+  def cancel(): Unit =
+    doOnUnderlying(_.cancel())
+
+  private def doOnUnderlying(f: Subscription ⇒ Unit): Unit =
+    Option(underlying.get()) match {
+      case Some(s) ⇒ f(s)
+      case None    ⇒ stash = stash enqueue f
+    }
+
+  @tailrec
+  private def unstash(q: Queue[Subscription ⇒ Unit], s: Subscription): Unit =
+    if (q.nonEmpty) {
+      val (head, tail) = q.dequeue
+      head(s)
+      unstash(tail, s)
+    }
+    else {
+      stash = q
+    }
+}
--- modules.sbt
+++ modules.sbt
@@ -1,6 +1,10 @@
 import JmhKeys._
+import scalariform.formatter.preferences._
 
 lazy val root = project.in(file("."))
+  .settings(libraryDependencies ++= List(
+    "org.scalatest"  %% "scalatest"  % "2.2.2" % "test",
+    "org.scalacheck" %% "scalacheck" % "1.11.6" % "test"))
 
 lazy val benchmark = project
   .dependsOn(root)
@@ -8,3 +12,19 @@ lazy val benchmark = project
   .settings(outputTarget in Jmh := target.value / s"scala-${scalaBinaryVersion.value}")
   .settings(libraryDependencies ++= List(
     "com.cognitect" % "transducers-java" % "0.4.67"))
+
+lazy val reactiveStreams = project
+  .in(file("contrib") / "reactive-streams")
+  .dependsOn(root)
+  .settings(scalariformSettings: _*)
+  .settings(ScalariformKeys.preferences := ScalariformKeys.preferences.value
+    .setPreference(AlignParameters, true)
+    .setPreference(AlignSingleLineCaseStatements, true)
+    .setPreference(CompactControlReadability, true)
+    .setPreference(DoubleIndentClassDeclaration, true)
+    .setPreference(PreserveDanglingCloseParenthesis, true)
+    .setPreference(RewriteArrowSymbols, true))
+  .settings(libraryDependencies ++= List(
+    "org.reactivestreams" % "reactive-streams" % "0.4.0",
+    "org.scalatest"  %% "scalatest"  % "2.2.2" % "test",
+    "com.typesafe.akka" %% "akka-stream-experimental" % "0.10-M1" % "test" exclude("org.reactivestreams", "reactive-streams")))
